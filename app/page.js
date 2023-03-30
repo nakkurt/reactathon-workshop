98cd8fb5bea7624b0c8a6602436582fa820db28f
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2023-03-30 15:23:52 +0000

    Init app

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import styles from '../styles/home.module.css'
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Mux Reactathon Workshop</h1>
+
+      <Link href="/upload">Upload</Link>
+      <br />
+      <Link href="/videos">All videos</Link>
+      <br />
+      <Link href="/videos">Single video</Link>
+
+      <hr className={styles.hr} />
+    </main>
+  )
+}
+
+export default Home
---
+++ app/upload/page.js
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default function Upload() {
+    return <h1>Upload page</h1>;
+}
