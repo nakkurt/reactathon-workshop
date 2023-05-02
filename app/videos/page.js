68ce85d42f54fe34c8127a9e4e33f291afcd0281
Liam Lindner <liam@mux.com>
2023-05-02 16:59:46 -0700

    Use client-side asset fetching

--- app/videos/page.js
+++ app/videos/page.js
@@ -1,9 +1,26 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { listAssets } from '../../server-lib/Mux.js'
 import AboutVideo from './AboutVideo.js'
 
-export default async function VideosPage() {
-  const assets = await listAssets()
+export default function VideosPage() {
+  const [assets, setAssets] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchAssets() {
+      const response = await fetch('/api/videos')
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const data = await response.json()
+      setAssets(data)
+      setLoading(false)
+    }
+    fetchAssets()
+  }, [])
+
   return (
     <>
       <Link href="/" className="link-home">
@@ -11,20 +28,24 @@ export default async function VideosPage() {
       </Link>
       <h1>All Videos</h1>
       <ul className="videos">
-        {assets.map((asset) => {
-          const assetId = asset.id
-          const playbackId = asset.playback_ids[0].id
-          return (
-            <li key={assetId}>
-              <img
-                src={`https://image.mux.com/${playbackId}/thumbnail.jpg?width=640&height=360`}
-                width={160}
-                height={90}
-              />
-              <AboutVideo asset={asset} />
-            </li>
-          )
-        })}
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          assets.map((asset) => {
+            const assetId = asset.id
+            const playbackId = asset.playback_ids[0].id
+            return (
+              <li key={assetId}>
+                <img
+                  src={`https://image.mux.com/${playbackId}/thumbnail.jpg?width=640&height=360`}
+                  width={160}
+                  height={90}
+                />
+                <AboutVideo asset={asset} />
+              </li>
+            )
+          })
+        )}
       </ul>
     </>
   )
